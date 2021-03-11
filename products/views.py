adc06d8310fb8a618afe0bccb0f7393a74911eab
Thiago Souza <thiagoaugustsouza@gmail.com>
2021-03-11 14:06:02 -0300

    adicao de views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'qs': queryset
+    }
+    return render(request, "products/list.html", context)
