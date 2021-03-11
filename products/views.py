69d8dab06b394262c61cfa80009de5e6ffafa9b5
Thiago Souza <thiagoaugustsouza@gmail.com>
2021-03-11 14:41:41 -0300

    atualizacao de metodo de view products

--- products/views.py
+++ products/views.py
@@ -3,13 +3,12 @@ from django.shortcuts import render
 
 from .models import Product
 
+#Class Based View
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
-
-def product_list_view(request):
-    queryset = Product.objects.all()
-    context = {
-        'qs': queryset
-    }
-    return render(request, "products/list.html", context)
+    template_name = "products/list.html"     
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
