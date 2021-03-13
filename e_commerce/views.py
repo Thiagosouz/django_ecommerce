453e239e67547b086e944ddef2f1a1641cf26a43
Thiago Souza <thiagoaugustsouza@gmail.com>
2021-03-13 12:57:43 -0300

    navbar implementado

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -23,8 +23,8 @@ def about_page(request):
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
     context = {
-                    "title": "Contact Page",
-                    "content": "Bem vindo a Contact Page",
+                    "title": "Fale conosco",
+                    "content": "Bem vindo a página de contato",
                     "form": contact_form	
               }
     if contact_form.is_valid():
